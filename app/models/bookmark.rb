6134fb9217475baa790f438be52e662719a09454
manuelle augereau <manuelleaugereau@gmail.com>
2025-05-29 14:12:46 +0200

    add validations to models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
+  # validates :list_id, uniqueness: { scope: :list }
 end
