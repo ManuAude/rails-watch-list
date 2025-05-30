d36e1d0c62db6c8ca2dc3e4700f181dce742af42
manuelle augereau <manuelleaugereau@gmail.com>
2025-05-30 11:20:37 +0200

    add controller bookmarks and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:new, :create, :show, :index]
+  root to: 'lists#index'
+  resources :lists, only: [:new, :create, :show, :index] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
